9598a17b0b20673ad415d808b76924c17afbc6e7
Igor Podsekin <ipodsekin@sdev.pro>
2015-10-23 18:19:21 +0300

    [*] Легкий рефакторинг

---
+++ sdev.phonecodes/Operator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sdev.phonecodes
+{
+    public class Operator
+    {
+        public Int64 From { get; set; }
+        public Int64 To { get; set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
+    }
+}
--- sdev.phonecodes/Program.cs
+++ sdev.phonecodes/Program.cs
@@ -4,56 +4,15 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace sdev.phonecodes
 {
-    public class Operator
-    {
-        public Int64 From { get; set; }
-        public Int64 To { get; set; }
-        public string Name { get; set; }
-        public string Region { get; set; }
-    }
-
     class Program
     {
-        static void Main(string[] args)
+        static void FillDatabase(SortedList<Int64, Operator> numbers)
         {
-            Console.WriteLine("Поиск операторов и телефонов по базе DEF. (c) 2015 Игорь Подсекин. wonderu@wonderu.com");
-            if (args.Length != 3)
-            {
-                Console.WriteLine("Необходимо указать параметры запуска: <полный путь до Excel-файла с номерами> <номер колонки с телефонными номерами> <номер колонки, с которой будут записываться названия операторов и регионы>");
-                return;
-            }
-            var fileInfoIn = new FileInfo(args[0]);
-            if (!fileInfoIn.Exists)
-            {
-                Console.WriteLine("Указанный файл не существует");
-                return;
-            }
-
-            int numIndex;
-
-
-            if (!(Int32.TryParse(args[1], out numIndex) && numIndex > 0))
-            {
-                Console.WriteLine("Номер колонки с телефонными номерами должен быть целым числом больше нуля");
-                return;
-            }
-
-            int resultIndex;
-            if (!(Int32.TryParse(args[2], out resultIndex) && resultIndex > 0))
-            {
-                Console.WriteLine("номер колонки, с которой будут записываться названия операторов и регионы, должен быть целым числом больше нуля");
-                return;
-            }
-
-            SortedList<Int64, Operator> numbers = new SortedList<Int64, Operator>();
-
             var dir = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
-            
+
             var files = Directory.EnumerateFiles(dir + "\\codes", "*.csv");
             foreach (var file in files)
             {
@@ -78,7 +37,10 @@ namespace sdev.phonecodes
                     }
                 }
             }
+        }
 
+        static void ProcessNumbers(SortedList<Int64, Operator> numbers, int numIndex, int resultIndex, FileInfo fileInfoIn)
+        {
             using (var package = new ExcelPackage(fileInfoIn))
             {
                 ExcelWorkbook workBook = package.Workbook;
@@ -93,7 +55,8 @@ namespace sdev.phonecodes
                             Int64 inNumber;
                             if (Int64.TryParse(currentWorksheet.Cells[i, numIndex].Value.ToString(), out inNumber))
                             {
-                                inNumber = inNumber % 10000000000;
+                                inNumber = inNumber % 10000000000; //сокращаем до 10 цифр
+
                                 var oper = numbers.FirstOrDefault(k => k.Value.From <= inNumber && k.Value.To >= inNumber);
                                 if (oper.Value != null)
                                 {
@@ -106,6 +69,43 @@ namespace sdev.phonecodes
                 }
                 package.Save();
             }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Поиск операторов и телефонов по базе DEF. (c) 2015 Игорь Подсекин. wonderu@wonderu.com");
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Необходимо указать параметры запуска: <полный путь до Excel-файла с номерами> <номер колонки с телефонными номерами> <номер колонки, с которой будут записываться названия операторов и регионы>");
+                return;
+            }
+            var fileInfoIn = new FileInfo(args[0]);
+            if (!fileInfoIn.Exists)
+            {
+                Console.WriteLine("Указанный файл не существует");
+                return;
+            }
+
+            int numIndex;
+
+            if (!(Int32.TryParse(args[1], out numIndex) && numIndex > 0))
+            {
+                Console.WriteLine("Номер колонки с телефонными номерами должен быть целым числом больше нуля");
+                return;
+            }
+
+            int resultIndex;
+            if (!(Int32.TryParse(args[2], out resultIndex) && resultIndex > 0))
+            {
+                Console.WriteLine("номер колонки, с которой будут записываться названия операторов и регионы, должен быть целым числом больше нуля");
+                return;
+            }
+
+            SortedList<Int64, Operator> numbers = new SortedList<Int64, Operator>();
+
+            FillDatabase(numbers);
+
+            ProcessNumbers(numbers, numIndex, resultIndex, fileInfoIn);
         }
     }
 }
